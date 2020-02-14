20f66c71b68455254e54e2f993a63d8cdc2f1ec0
ApChris <achris.info@gmail.com>
2020-02-14 13:46:26 +0200

    queue.h

---
+++ include/queue.h
@@ -0,0 +1,22 @@
+#ifndef __QUEUE_H__
+#define __QUEUE_H__
+
+typedef struct Node
+{
+    long data;
+    struct Node * next;
+}Node;
+
+
+typedef struct Queue
+{
+    Node * front;
+    Node * rear;
+}Queue;
+
+Node * CreateNode(long data);
+Queue * CreateQueue();
+void PushQueue(Queue * queue, long data);
+void PopQueue(Queue * queue);
+
+#endif
